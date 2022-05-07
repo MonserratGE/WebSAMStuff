b2a9aa9ed7224c0d6c3ba7973625f1240bb77280
Manuad <manuel.adan4@gmail.com>
2022-05-06 23:41:57 -0500

    Consumiendo API de muebles

---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+const myAccess = '563492ad6f91700001000001189ff3f514204c2e8051b9e549c2158b';
+const url = 'https://api.pexels.com/v1/search?query=furniture&per_page=15';
+const imageContainer = document.getElementById('imageContainer');
+
+fetch(url, {
+    headers: {
+        Authorization: myAccess
+    }
+    }).
+    then(
+        (response) => response.json()
+        ).
+        then(
+            (data) => {
+                const arrayImages = data.photos.slice(0, 9);
+
+                arrayImages.forEach(image => {
+                    const datosNecesarios = {
+                        image: image.src.tiny,
+                        title: image.photographer,
+                    }
+
+                    const template = `
+                    <div  class="col-12">      
+                        <div class="card mt-3 mb-3" role="button">
+                            <img src="${datosNecesarios.image}" class="card-img-top img-card" alt="..."/>
+                            <div class="card-body">
+                                <div class="d-flex flex-row bb-2">
+                                    <h5 class="card-title ">${datosNecesarios.title}</h5>
+                                    <a class="ml-auto card-title " href="#"> <i class="fas fa-shopping-cart fa-2x go-shopping go-shopping"></i> </a>            
+                                </div>
+                                    <p class="card-text mb-3">
+                                        This is a longer card with supporting text below as a natural
+                                        lead-in to additional content. This content is a little bit
+                                        longer.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    `;
+                    imageContainer.innerHTML += template;
+                });
+            });
