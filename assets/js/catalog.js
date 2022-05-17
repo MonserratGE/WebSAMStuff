afe30223ab6a8f5b06c27cd75c130a40e5e9d962
Manuad <manuel.adan4@gmail.com>
2022-05-16 23:04:28 -0500

    Agregando la API propia de muestra correctamente en catalogo.html

--- assets/js/catalog.js
+++ assets/js/catalog.js
@@ -1,42 +1,33 @@
-const myAccess = '563492ad6f91700001000001189ff3f514204c2e8051b9e549c2158b';
-const url = 'https://api.pexels.com/v1/search?query=furniture&per_page=80';
+//const myAccess = '563492ad6f91700001000001189ff3f514204c2e8051b9e549c2158b';
+const url = '../src/api/datamuebles.json';
 const catalogContainer = document.getElementById('catalogContainer');
 
-fetch(url, {
-    headers: {
-        Authorization: myAccess
-    }
-    }).
-    then(
-        (response) => response.json()
-        ).
+fetch(url).then((response) => response.json()).
         then(
             (data) => {
-            const arrayImages = data.photos.slice(0, 80);
+            const arrayProducts = data.productos.slice(0, 25);
 
-            arrayImages.forEach(image => {
+            arrayProducts.forEach(product => {
                 const datosNecesarios = {
-                    id: image.id,
-                    image: image.src.tiny,
-                    title: image.photographer,
-                    alt: image.alt
+                    id: product.id,
+                    nombre: product.nombre,
+                    descripcion: product.descripcion,
+                    front_image: product.imagen_frontal,
                 }
 
                 const template = `
                 <div  class="col-12">      
                     <div class="card mt-3 mb-3" role="button">
                         <a href=#">
-                            <img src="${datosNecesarios.image}" class="card-img-top img-card" alt="${datosNecesarios.alt}"/>
+                            <img src="${datosNecesarios.front_image}" class="card-img-top img-card" alt="${datosNecesarios.alt}"/>
                         </a>
                         <div class="card-body">
                             <div class="d-flex flex-row bb-2">
-                                <h5 class="card-title ">${datosNecesarios.title}</h5>
+                                <h5 class="card-title ">${datosNecesarios.nombre}</h5>
                                 <a class="ml-auto card-title " href="carrito.html"> <i class="fas fa-shopping-cart fa-2x go-shopping go-shopping"></i> </a>            
                             </div>
                                 <p class="card-text mb-3">
-                                    This is a longer card with supporting text below as a natural
-                                    lead-in to additional content. This content is a little bit
-                                    longer.
+                                    ${datosNecesarios.descripcion}
                                 </p>
                             </div>
                         </div>
