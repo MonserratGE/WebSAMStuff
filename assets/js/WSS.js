ee7c5ccb2443b39723432fbf0c2715ec64fb83aa
MijhailCabrera <mijhailcabrera@gmail.com>
2022-05-27 04:55:33 -0500

    base de WSS en espera de indicaciones

---
+++ assets/js/WSS.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('#Inicio',{delay: 500});
+ScrollReveal().reveal('#video',{delay: 500});
+ScrollReveal().reveal('#adoptar',{delay: 500});
+ScrollReveal().reveal('#catalogContainer',{delay: 500});
