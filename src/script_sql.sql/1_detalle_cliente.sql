271a783525ea975a593759223dd7139e05a7135d
MonserratGE <monserrat.garcia.espinoza32@gmail.com>
2022-06-10 16:54:51 -0500

    Eliminando bases

--- src/script_sql.sql/1_detalle_cliente.sql
+++
@@ -1,5 +0,0 @@
-INSERT into detalle_cliente values (1, "paseo de los nublados, 776,  la roma, CDMX ", "5578989878","memito@gmail.com","5566242378656745", "Guillermo Perez Canela","0927", NOW(),NOW());
-
-UPDATE detalle_cliente set numero_tarjeta = "5566242378659878", updated_at = now() where id_detalle_cliente = 1;
-
-DELETE FROM detalle_cliente WHERE id_detalle_cliente = 2;
